90748dc19b60cbb6c134467ed4415f754753651c
yinyunqi <yinyunqi@DESKTOP-CQ89CJF>
2018-09-14 10:07:30 +0800

    allqq

---
+++ Damionew/src/main/java/com/damionew/DamionewApplication.java
@@ -0,0 +1,12 @@
+package com.damionew;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DamionewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DamionewApplication.class, args);
+	}
+}
